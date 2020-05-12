778c005d70eb415123c48c99bf78312e41d96d56
Stephen Printup <stephen.printup@gmail.com>
2020-05-12 09:08:13 -0600

    decorator with books and movies examples

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Decorator
 {
@@ -7,6 +8,22 @@ namespace Decorator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! decorator");
+            // Real-World C#
+            Book book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            Video video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            Console.WriteLine("\nMaking video borrowable");
+
+            Borrowable borrowvideo = new Borrowable(video);
+            borrowvideo.BorrowItem("Customer #1");
+            borrowvideo.BorrowItem("Customer #2");
+
+            borrowvideo.Display();
+
+            // Structural C#
             ConcreteComponent c = new ConcreteComponent();
             ConcreteDecoratorA d1 = new ConcreteDecoratorA();
             ConcreteDecoratorB d2 = new ConcreteDecoratorB();
@@ -20,9 +37,66 @@ namespace Decorator
         }
     }
 
+    abstract class LibraryItem
+    {
+        private int _numCopies;
+
+        public int NumCopies
+        {
+            get { return _numCopies; }
+            set { _numCopies = value; }
+        }
+        public abstract void Display();
+    }
+
     abstract class Component
     {
         public abstract void Operation();
+    }
+
+
+    class Book : LibraryItem
+    {
+        private string _author;
+        private string _title;
+
+        public Book(string author, string title, int numCopies)
+        {
+            this._author = author;
+            this._title = title;
+            this.NumCopies = numCopies;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\nBook ------ ");
+            Console.WriteLine(" Author: {0}", _author);
+            Console.WriteLine(" Title: {0}", _title);
+            Console.WriteLine(" # Copies: {0}", NumCopies);
+        }
+    }
+
+    class Video : LibraryItem
+    {
+        private string _director;
+        private string _title;
+        private int _playTime;
+
+        public Video(string director, string title, int numCopies, int playTime)
+        {
+            this._director = director;
+            this._title = title;
+            this.NumCopies = numCopies;
+            this._playTime = playTime;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\n Video -------");
+            Console.WriteLine(" Director: {0}", _director);
+            Console.WriteLine(" Title: {0}",_title);
+            Console.WriteLine(" # Copies: {0}", NumCopies);
+            Console.WriteLine(" Playtime: {0}\n", _playTime);
+        }
     }
 
     class ConcreteComponent : Component
@@ -33,7 +107,20 @@ namespace Decorator
         }
     }
 
-    abstract class Decorator : Component
+    abstract class Decorator : LibraryItem
+    {
+        protected LibraryItem libraryItem;
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+    }
+
+    abstract class Decorator1 : Component
     {
         protected Component component;
 
@@ -51,7 +138,33 @@ namespace Decorator
         }
     }
 
-    class ConcreteDecoratorA : Decorator
+    class Borrowable : Decorator
+    {
+        protected List<string> borrowers = new List<string>();
+
+        public Borrowable(LibraryItem libraryItem) : base(libraryItem) { }
+
+        public void BorrowItem(string name)
+        {
+            borrowers.Add(name);
+            libraryItem.NumCopies--;
+        }
+        public void ReturnItem(string name)
+        {
+            borrowers.Remove(name);
+            libraryItem.NumCopies++;
+        }
+        public override void Display()
+        {
+            base.Display();
+            foreach (string borrower in borrowers)
+            {
+                Console.WriteLine(" borrower: " + borrower);
+            }
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator1
     {
         public override void Operation()
         {
@@ -59,7 +172,7 @@ namespace Decorator
             Console.WriteLine("ConcreteDecoratorA.Operation()");
         }
     }
-    class ConcreteDecoratorB : Decorator
+    class ConcreteDecoratorB : Decorator1
     {
         public override void Operation()
         {
