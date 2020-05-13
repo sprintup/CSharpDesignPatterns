5d16e3952ffc45900a10f062410b38c8137d6df2
Stephen Printup <stephen.printup@gmail.com>
2020-05-13 09:55:13 -0600

    working version of flyweight

---
+++ Flyweight/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! flyweight");
+            int extrinsicstate = 22;
+
+            FlyweightFactory factory = new FlyweightFactory();
+
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight fu = new UnsharedConcreteFlyweight();
+
+            fu.Operation(--extrinsicstate);
+
+            Console.ReadKey();
+        }
+    }
+
+    class FlyweightFactory
+    {
+        private Hashtable flyweights = new Hashtable();
+
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            return ((Flyweight)flyweights[key]);
+        }
+    }
+
+    abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+
+    class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+
+    class UnsharedConcreteFlyweight: Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("UnsharedConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+}
