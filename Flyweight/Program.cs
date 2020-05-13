69a13e08a37b68642a5f9dc4db8dd173544f0370
Stephen Printup <stephen.printup@gmail.com>
2020-05-13 10:27:48 -0600

    flyweight working with characater objects

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Flyweight
 {
@@ -8,17 +9,33 @@ namespace Flyweight
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! flyweight");
+            //Real world C#
+            string document = "AAZZBBZB";
+            char[] chars = document.ToCharArray();
+
+            CharacterFactory factory = new CharacterFactory();
+
+            int pointSize = 10;
+
+            foreach (char c in chars)
+            {
+                pointSize++;
+                Character character = factory.GetCharacter(c);
+                character.Display(pointSize);
+            }
+
+            //Structural C#
             int extrinsicstate = 22;
 
-            FlyweightFactory factory = new FlyweightFactory();
+            FlyweightFactory factory1 = new FlyweightFactory();
 
-            Flyweight fx = factory.GetFlyweight("X");
+            Flyweight fx = factory1.GetFlyweight("X");
             fx.Operation(--extrinsicstate);
 
-            Flyweight fy = factory.GetFlyweight("Y");
+            Flyweight fy = factory1.GetFlyweight("Y");
             fy.Operation(--extrinsicstate);
 
-            Flyweight fz = factory.GetFlyweight("Z");
+            Flyweight fz = factory1.GetFlyweight("Z");
             fz.Operation(--extrinsicstate);
 
             UnsharedConcreteFlyweight fu = new UnsharedConcreteFlyweight();
@@ -26,6 +43,31 @@ namespace Flyweight
             fu.Operation(--extrinsicstate);
 
             Console.ReadKey();
+        }
+    }
+
+    class CharacterFactory
+    {
+        private Dictionary<char, Character> _characters = new Dictionary<char, Character>();
+
+        public Character GetCharacter(char key)
+        {
+            Character character = null;
+            if (_characters.ContainsKey(key))
+            {
+                character = _characters[key];
+            }
+            else
+            {
+                switch (key)
+                {
+                    case 'A': character = new CharacterA(); break;
+                    case 'B': character = new CharacterB(); break;
+                    case 'Z': character = new CharacterZ(); break;
+                }
+                _characters.Add(key, character);
+            }
+            return character;
         }
     }
 
@@ -46,9 +88,75 @@ namespace Flyweight
         }
     }
 
+    abstract class Character
+    {
+        protected char symbol;
+        protected int width;
+        protected int height;
+        protected int ascent;
+        protected int descent;
+        protected int pointSize;
+
+        public abstract void Display(int pointSize);
+    }
+
     abstract class Flyweight
     {
         public abstract void Operation(int extrinsicstate);
+    }
+
+    class CharacterA : Character
+    {
+        public CharacterA()
+        {
+            this.symbol = 'A';
+            this.height = 100;
+            this.width = 120;
+            this.ascent = 70;
+            this.descent = 0;
+        }
+
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+        }
+    }
+
+    class CharacterB: Character
+    {
+        public CharacterB()
+        {
+            this.symbol = 'B';
+            this.height = 100;
+            this.width = 140;
+            this.ascent = 72;
+            this.descent = 0;
+        }
+
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+        }
+    }
+
+    class CharacterZ : Character
+    {
+        public CharacterZ()
+        {
+            this.symbol = 'Z';
+            this.height = 100;
+            this.width = 100;
+            this.ascent = 68;
+            this.descent = 0;
+        }
+
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+        }
     }
 
     class ConcreteFlyweight : Flyweight
