6b8f9d2f57c7fa42671701f8112f786e2fe9fac4
Stephen Printup <stephen.printup@gmail.com>
2020-05-14 17:28:27 -0600

    proxy is working

---
+++ Proxy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! proxy");
+
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+}
