c0efaa413f78c1fb040847fdff417b62ed8c3a59
Stephen Printup <stephen.printup@gmail.com>
2020-05-14 18:19:44 -0600

    chain of responsibility working with approvers

---
+++ Chain of Responsibility/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace Chain_of_Responsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Chain of Responsibility");
+            //Real-world code in C#
+            Approver larry = new Director();
+            Approver sam = new VicePresident();
+            Approver tammy = new President();
+
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+
+            Purchase p = new Purchase(2034, 350.00, "Assets");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p);
+            
+            //Structural C# Code
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            Handler h3 = new ConcreteHandler3();
+            h1.SetSuccessor(h2);
+            h2.SetSuccessor(h3);
+
+            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+
+            foreach (int request in requests)
+            {
+                h1.HandleRequest(request);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    //'Handler' abstract class
+    abstract class Approver
+    {
+        protected Approver successor;
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+
+    abstract class Handler
+    {
+        protected Handler successor;
+
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+
+    // The 'ConcreteHandler class
+    class Director : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 10000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+    class VicePresident : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 25000.0)
+            {
+                Console.WriteLine("{0} approved request#{1}", this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+    
+    class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 100000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+            }
+            else
+            {
+                Console.WriteLine("Request# {0} requires an executive meeting!", purchase.Number);
+            }
+        }
+    }
+
+    class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine("{0} handled request {1}", this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 10 && request < 20)
+            {
+                Console.WriteLine("{0} handled request {1}", this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandler3 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 20 && request < 30)
+            {
+                Console.WriteLine("{0} handled request {1}", this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    // class holding request details
+
+    class Purchase
+    {
+        private int _number;
+        private double _amount;
+        private string _purpose;
+
+        public Purchase(int number, double amount, string purpose)
+        {
+            this._number = number;
+            this._amount = amount;
+            this._purpose = purpose;
+        }
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value;  }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public string Purpose
+        {
+            get { return _purpose; }
+            set { _purpose = value; }
+        }
+    }
+}
