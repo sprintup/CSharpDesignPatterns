6e6ea875ab9e114d169e4c25073a5ffb93911422
Stephen Printup <stephen.printup@gmail.com>
2020-05-21 16:45:42 -0600

    iterating over objects using real world example

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -8,14 +8,39 @@ namespace Iterator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Iterator");
+            //Real-world C#
 
+            Collection collection = new Collection();
+            collection[0] = new Item("Item 0");
+            collection[1] = new Item("Item 1");
+            collection[2] = new Item("Item 2");
+            collection[3] = new Item("Item 3");
+            collection[4] = new Item("Item 4");
+            collection[5] = new Item("Item 5");
+            collection[6] = new Item("Item 6");
+            collection[7] = new Item("Item 7");
+            collection[8] = new Item("Item 8");
+
+            Iterator iterator = collection.CreateIterator();
+
+            iterator.Step = 2;
+
+            Console.WriteLine("Iterating over collection");
+
+            for (Item item1 = iterator.First();
+                !iterator.IsDone; item1 = iterator.Next())
+            {
+                Console.WriteLine(item1.Name);
+            }
+
+            // Structural C#
             ConcreteAggregate a = new ConcreteAggregate();
             a[0] = "Item A";
             a[1] = "Item B";
             a[2] = "Item C";
             a[3] = "Item D";
 
-            Iterator i = a.CreateIterator();
+            Iterator1 i = a.CreateIterator();
 
             Console.WriteLine("Iterating over collection");
 
@@ -29,17 +54,51 @@ namespace Iterator
             Console.ReadKey();
         }
     }
-
+    // The collection item
+    class Item
+    {
+        private string _name;
+        public Item(string name)
+        {
+            this._name = name;
+        }
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+    // The 'Aggregate' interface
+    interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
     abstract class Aggregate
     {
-        public abstract Iterator CreateIterator();
+        public abstract Iterator1 CreateIterator();
     }
-
+    // The 'ConcreteAggregate' class
+    class Collection : IAbstractCollection
+    {
+        private ArrayList _items = new ArrayList();
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Add(value); }
+        }
+    }
     class ConcreteAggregate : Aggregate
     {
         private ArrayList _items = new ArrayList();
 
-        public override Iterator CreateIterator()
+        public override Iterator1 CreateIterator()
         {
             return new ConcreteIterator(this);
         }
@@ -55,16 +114,63 @@ namespace Iterator
             set { _items.Insert(index, value); }
         }
     }
-
-    abstract class Iterator
+    // The 'Iterator' interface
+    interface IAbstractIterator
+    {
+        Item First();
+        Item Next();
+        bool IsDone { get; }
+        Item CurrentItem { get; }
+    }
+    abstract class Iterator1
     {
         public abstract object First();
         public abstract object Next();
         public abstract bool IsDone();
         public abstract object CurrentItem();
     }
+    // The 'ConcreteIterator' class
+    class Iterator : IAbstractIterator
+    {
+        private Collection _collection;
+        private int _current = 0;
+        private int _step = 1;
 
-    class ConcreteIterator : Iterator
+        public Iterator(Collection collection)
+        {
+            this._collection = collection;
+        }
+        public Item First()
+        {
+            _current = 0;
+            return _collection[_current] as Item;
+        }
+        public Item Next()
+        {
+            _current += _step;
+            if (!IsDone)
+                return _collection[_current] as Item;
+            else
+                return null;
+        }
+
+        public int Step
+        {
+            get { return _step; }
+            set { _step = value; }
+        }
+
+        public Item CurrentItem
+        {
+            get { return _collection[_current] as Item; }
+        }
+
+        public bool IsDone
+        {
+            get { return _current >= _collection.Count; }
+        }
+    }
+    class ConcreteIterator : Iterator1
     {
         private ConcreteAggregate _aggregate;
         private int _current = 0;
