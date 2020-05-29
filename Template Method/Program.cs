c13843433d95056dc73d9086602aebd90f331620
Stephen Printup <stephen.printup@gmail.com>
2020-05-29 12:04:57 -0600

    Template method working, but with a duplicate that might be a problem with the source

---
+++ Template Method/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Template_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Template Method");
+
+            AbstractClass aA = new ConcreteClassA();
+            aA.TemplateMethod();
+
+            AbstractClass aB = new ConcreteClassB();
+            aB.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine("");
+        }
+    }
+
+    class ConcreteClassA : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+        }
+    }
+
+    class ConcreteClassB : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+        }
+    }
+}
