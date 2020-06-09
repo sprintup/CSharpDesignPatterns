eacf40464aac567361ecb3373f8bc4fcc6bb92ca
Stephen Printup <corky@corkysdevshop.com>
2020-06-09 08:45:41 -0600

    refactored prototype into new classes

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -8,99 +8,10 @@ namespace Prototype
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! prototype");
-            //new
-            ColorManager colormanager = new ColorManager();
-            colormanager["red"] = new Color(255, 0, 0);
-            colormanager["green"] = new Color(0, 255, 0);
-            colormanager["blue"] = new Color(0, 0, 255);
-
-            colormanager["angry"] = new Color(255, 54, 0);
-            colormanager["peace"] = new Color(128, 211, 128);
-            colormanager["flame"] = new Color(211, 34, 20);
-
-            Color color1 = colormanager["red"].Clone() as Color;
-            Color color2 = colormanager["peace"].Clone() as Color;
-            Color color3 = colormanager["flame"].Clone() as Color;
-
-            Console.WriteLine("-----");
-            //old
-            ConcretePrototype1 p1 = new ConcretePrototype1("I");
-            ConcretePrototype1 c1 = (ConcretePrototype1)p1.Clone();
-
-            Console.WriteLine("Cloned: {0}", c1.Id);
-            ConcretePrototype2 p2 = new ConcretePrototype2("II");
-            ConcretePrototype2 c2 = (ConcretePrototype2)p2.Clone();
-            Console.WriteLine("Cloned: {0}", c2.Id);
+            Prototype_Structural.Run();
+            Prototype_RealWorld.Run();
 
             Console.ReadKey();
-        }
-    }
-    //new
-    abstract class ColorPrototype
-    {
-        public abstract ColorPrototype Clone();
-    }
-
-    class Color : ColorPrototype
-    {
-        private int _red;
-        private int _green;
-        private int _blue;
-
-        public Color(int red, int green, int blue)
-        {
-            this._red = red;
-            this._green = green;
-            this._blue = blue;
-        }
-
-        public override ColorPrototype Clone()
-        {
-            Console.WriteLine("Cloning color RGB: {0,3},{1,3},{2,3}", _red, _green, _blue);
-            return this.MemberwiseClone() as ColorPrototype;
-        }
-    }
-
-    class ColorManager
-    {
-        private Dictionary<string, ColorPrototype> _colors = new Dictionary<string, ColorPrototype>();
-
-        public ColorPrototype this[string key]
-        {
-            get { return _colors[key]; }
-            set { _colors.Add(key, value); }
-        }
-    }
-    //old
-    abstract class Prototype
-    {
-        private string _id;
-        public Prototype(string id)
-        {
-            this._id = id;
-        }
-        public string Id
-        {
-            get { return _id; }
-        }
-        public abstract Prototype Clone();
-    }
-
-    class ConcretePrototype1 : Prototype
-    {
-        public ConcretePrototype1(string id) : base(id) { }
-        public override Prototype Clone()
-        {
-            return (Prototype)this.MemberwiseClone();
-        }
-    }
-
-    class ConcretePrototype2 : Prototype
-    {
-        public ConcretePrototype2(string id) : base(id) { }
-        public override Prototype Clone()
-        {
-            return (Prototype)this.MemberwiseClone();
         }
     }
 }
---
+++ Prototype/Prototype_RealWorld.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    class Prototype_RealWorld
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Prototype Real World");
+            ColorManager colormanager = new ColorManager();
+            colormanager["red"] = new Color(255, 0, 0);
+            colormanager["green"] = new Color(0, 255, 0);
+            colormanager["blue"] = new Color(0, 0, 255);
+
+            colormanager["angry"] = new Color(255, 54, 0);
+            colormanager["peace"] = new Color(128, 211, 128);
+            colormanager["flame"] = new Color(211, 34, 20);
+
+            Color color1 = colormanager["red"].Clone() as Color;
+            Color color2 = colormanager["peace"].Clone() as Color;
+            Color color3 = colormanager["flame"].Clone() as Color;
+        }
+        abstract class ColorPrototype
+        {
+            public abstract ColorPrototype Clone();
+        }
+
+        class Color : ColorPrototype
+        {
+            private int _red;
+            private int _green;
+            private int _blue;
+
+            public Color(int red, int green, int blue)
+            {
+                this._red = red;
+                this._green = green;
+                this._blue = blue;
+            }
+
+            public override ColorPrototype Clone()
+            {
+                Console.WriteLine("Cloning color RGB: {0,3},{1,3},{2,3}", _red, _green, _blue);
+                return this.MemberwiseClone() as ColorPrototype;
+            }
+        }
+
+        class ColorManager
+        {
+            private Dictionary<string, ColorPrototype> _colors = new Dictionary<string, ColorPrototype>();
+
+            public ColorPrototype this[string key]
+            {
+                get { return _colors[key]; }
+                set { _colors.Add(key, value); }
+            }
+        }
+    }
+}
