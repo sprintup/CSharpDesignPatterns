2b5626ca3bbcb0ec0373828dafa74af5d493c018
Stephen Printup <corky@corkysdevshop.com>
2020-06-10 09:52:20 -0600

    refactored adapter into their own classes

---
+++ Adapter/Adapter_Structural.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    class Adapter_Structural
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Structural");
+            Target target = new Adapter();
+            target.Request();
+        }
+
+        class Target
+        {
+            public virtual void Request()
+            {
+                Console.WriteLine("Called Target Request()");
+            }
+        }
+
+        class Adapter : Target
+        {
+            private Adaptee _adaptee = new Adaptee();
+
+            public override void Request()
+            {
+                _adaptee.SpecificRequest();
+            }
+        }
+
+        class Adaptee
+        {
+            public void SpecificRequest()
+            {
+                Console.WriteLine("Called SpecificRequst()");
+            }
+        }
+    }
+}
