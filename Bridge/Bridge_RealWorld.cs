03d83b3942ef5017e52e468b0b3b89cfedc33c2a
Stephen Printup <corky@corkysdevshop.com>
2020-06-10 10:01:35 -0600

    Bridge Refactored into own method

---
+++ Bridge/Bridge_RealWorld.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    class Bridge_RealWorld
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Real World");
+            Customers customers = new Customers("Chicago");
+            customers.Data = new CustomersData();
+
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Add("Henry Velasquez");
+
+            customers.ShowAll();
+        }
+        class CustomersBase
+        {
+            private DataObject _dataObject;
+            protected string group;
+
+            public CustomersBase(string group)
+            {
+                this.group = group;
+            }
+
+            public DataObject Data
+            {
+                set { _dataObject = value; }
+                get { return _dataObject; }
+            }
+
+            public virtual void Next()
+            {
+                _dataObject.NextRecord();
+            }
+
+            public virtual void Prior()
+            {
+                _dataObject.PriorRecord();
+            }
+            public virtual void Add(string customer)
+            {
+                _dataObject.AddRecord(customer);
+            }
+            public virtual void Delete(string customer)
+            {
+                _dataObject.DeleteRecord(customer);
+            }
+            public virtual void Show()
+            {
+                _dataObject.ShowRecord();
+            }
+            public virtual void ShowAll()
+            {
+                Console.WriteLine("Customer Group: " + group);
+                _dataObject.ShowAllRecords();
+            }
+        }
+
+        class Customers : CustomersBase
+        {
+            public Customers(string group) : base(group) { }
+            public override void ShowAll()
+            {
+                Console.WriteLine();
+                Console.WriteLine("-------------------------------:");
+                base.ShowAll();
+                Console.WriteLine("-------------------------------:");
+            }
+        }
+
+        abstract class DataObject
+        {
+            public abstract void NextRecord();
+            public abstract void PriorRecord();
+            public abstract void AddRecord(string name);
+            public abstract void DeleteRecord(string name);
+            public abstract void ShowRecord();
+            public abstract void ShowAllRecords();
+        }
+
+        class CustomersData : DataObject
+        {
+            private List<string> _customers = new List<string>();
+            private int _current = 0;
+            public CustomersData()
+            {
+                _customers.Add("Jim Jones");
+                _customers.Add("Samual Jackson");
+                _customers.Add("Allen Good");
+                _customers.Add("Ann Stills");
+                _customers.Add("Lisa Giolani");
+            }
+            public override void NextRecord()
+            {
+                if (_current <= _customers.Count - 1)
+                {
+                    _current++;
+                }
+            }
+            public override void PriorRecord()
+            {
+                if (_current > 0)
+                {
+                    _current--;
+                }
+            }
+            public override void AddRecord(string customer)
+            {
+                _customers.Add(customer);
+            }
+            public override void DeleteRecord(string customer)
+            {
+                _customers.Remove(customer);
+            }
+            public override void ShowRecord()
+            {
+                Console.WriteLine(_customers[_current]);
+            }
+            public override void ShowAllRecords()
+            {
+                foreach (string customer in _customers)
+                {
+                    Console.WriteLine(" " + customer);
+                }
+            }
+        }
+    }
+}
---
+++ Bridge/Bridge_Structural.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    class Bridge_Structural
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Structural");
+            Abstraction ab = new RefinedAbstraction();
+
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+        }
+        class Abstraction
+        {
+            protected Implementor implementor;
+
+            public Implementor Implementor
+            {
+                set { implementor = value; }
+            }
+
+            public virtual void Operation()
+            {
+                implementor.Operation();
+            }
+        }
+
+        abstract class Implementor
+        {
+            public abstract void Operation();
+        }
+
+        class RefinedAbstraction : Abstraction
+        {
+            public override void Operation()
+            {
+                implementor.Operation();
+            }
+        }
+
+        class ConcreteImplementorA : Implementor
+        {
+            public override void Operation()
+            {
+                Console.WriteLine("ConcreteImplementorA Operation");
+            }
+        }
+
+        class ConcreteImplementorB : Implementor
+        {
+            public override void Operation()
+            {
+                Console.WriteLine("ConcreteImplementorB Operation");
+            }
+        }
+    }
+}
