8afa521e7e7b929b5addaf2025c7a8dfe9f16f9c
Stephen Printup <corky@corkysdevshop.com>
2020-06-11 10:54:25 -0600

    Flyweight refactored into its own classes

---
+++ Flyweight/Flyweight_RealWorld.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    class Flyweight_RealWorld
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This real-world code demonstrates the Flyweight pattern in which a relatively small number of Character objects is shared many times by a document that has potentially many characters.");
+            string document = "AAZZBBZB";
+            char[] chars = document.ToCharArray();
+
+            CharacterFactory factory = new CharacterFactory();
+
+            int pointSize = 10;
+
+            foreach (char c in chars)
+            {
+                pointSize++;
+                Character character = factory.GetCharacter(c);
+                character.Display(pointSize);
+            }
+            /*
+                A (pointsize 11)
+                A (pointsize 12)
+                Z (pointsize 13)
+                Z (pointsize 14)
+                B (pointsize 15)
+                B (pointsize 16)
+                Z (pointsize 17)
+                B (pointsize 18)
+             */
+        }
+        class CharacterFactory
+        {
+            private Dictionary<char, Character> _characters = new Dictionary<char, Character>();
+
+            public Character GetCharacter(char key)
+            {
+                Character character = null;
+                if (_characters.ContainsKey(key))
+                {
+                    character = _characters[key];
+                }
+                else
+                {
+                    switch (key)
+                    {
+                        case 'A': character = new CharacterA(); break;
+                        case 'B': character = new CharacterB(); break;
+                        case 'Z': character = new CharacterZ(); break;
+                    }
+                    _characters.Add(key, character);
+                }
+                return character;
+            }
+        }
+
+        abstract class Character
+        {
+            protected char symbol;
+            protected int width;
+            protected int height;
+            protected int ascent;
+            protected int descent;
+            protected int pointSize;
+
+            public abstract void Display(int pointSize);
+        }
+
+        class CharacterA : Character
+        {
+            public CharacterA()
+            {
+                this.symbol = 'A';
+                this.height = 100;
+                this.width = 120;
+                this.ascent = 70;
+                this.descent = 0;
+            }
+
+            public override void Display(int pointSize)
+            {
+                this.pointSize = pointSize;
+                Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+            }
+        }
+
+        class CharacterB : Character
+        {
+            public CharacterB()
+            {
+                this.symbol = 'B';
+                this.height = 100;
+                this.width = 140;
+                this.ascent = 72;
+                this.descent = 0;
+            }
+
+            public override void Display(int pointSize)
+            {
+                this.pointSize = pointSize;
+                Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+            }
+        }
+
+        class CharacterZ : Character
+        {
+            public CharacterZ()
+            {
+                this.symbol = 'Z';
+                this.height = 100;
+                this.width = 100;
+                this.ascent = 68;
+                this.descent = 0;
+            }
+
+            public override void Display(int pointSize)
+            {
+                this.pointSize = pointSize;
+                Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+            }
+        }
+    }
+}
---
+++ Flyweight/Flyweight_Structural.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    class Flyweight_Structural
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This structural code demonstrates the Flyweight pattern in which a relatively small number of objects is shared many times by different clients.");
+
+            int extrinsicstate = 22;
+
+            FlyweightFactory factory1 = new FlyweightFactory();
+
+            Flyweight fx = factory1.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = factory1.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = factory1.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight fu = new UnsharedConcreteFlyweight();
+
+            fu.Operation(--extrinsicstate);
+            /*
+                ConcreteFlyweight: 21
+                ConcreteFlyweight: 20
+                ConcreteFlyweight: 19
+                UnsharedConcreteFlyweight: 18
+             */
+        }
+
+        class FlyweightFactory
+        {
+            private Hashtable flyweights = new Hashtable();
+
+            public FlyweightFactory()
+            {
+                flyweights.Add("X", new ConcreteFlyweight());
+                flyweights.Add("Y", new ConcreteFlyweight());
+                flyweights.Add("Z", new ConcreteFlyweight());
+            }
+
+            public Flyweight GetFlyweight(string key)
+            {
+                return ((Flyweight)flyweights[key]);
+            }
+        }
+
+        abstract class Flyweight
+        {
+            public abstract void Operation(int extrinsicstate);
+        }
+
+        class ConcreteFlyweight : Flyweight
+        {
+            public override void Operation(int extrinsicstate)
+            {
+                Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+            }
+        }
+
+        class UnsharedConcreteFlyweight : Flyweight
+        {
+            public override void Operation(int extrinsicstate)
+            {
+                Console.WriteLine("UnsharedConcreteFlyweight: " + extrinsicstate);
+            }
+        }
+    }
+}
