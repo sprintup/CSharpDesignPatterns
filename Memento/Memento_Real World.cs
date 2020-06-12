948a6d9bc2f8e59ff63aa38444633273db9939df
Stephen Printup <corky@corkysdevshop.com>
2020-06-12 10:39:41 -0600

    memento refactored into its own classes

---
+++ Memento/Memento_Real World.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    class Memento_Real_World
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This real-world code demonstrates the Memento pattern which temporarily saves and then restores the SalesProspect's internal state.\n");
+            SalesProspect s = new SalesProspect();
+            s.Name = "Noel van Halen";
+            s.Phone = "(412) 256-0990";
+            s.Budget = 25000.0;
+
+            ProspectMemory m = new ProspectMemory();
+            m.Memento = s.SaveMemento();
+
+            s.Name = "Leo Welch";
+            s.Phone = "(310) 209-7111";
+            s.Budget = 1000000.0;
+
+            s.RestoreMemento(m.Memento);
+            Console.WriteLine("----");
+            /*
+            Name:   Noel van Halen
+            Phone:  (412) 256-0990
+            Budget: 25000
+
+            Saving state --
+
+            Name:   Leo Welch
+            Phone:  (310) 209-7111
+            Budget: 1000000
+
+            Restoring state --
+
+            Name:   Noel van Halen
+            Phone:  (412) 256-0990
+            Budget: 25000
+             */
+        }
+        // The originator class
+        class SalesProspect
+        {
+            private string _name;
+            private string _phone;
+            private double _budget;
+
+            public string Name
+            {
+                get { return _name; }
+                set
+                {
+                    _name = value;
+                    Console.WriteLine("Name: " + _name);
+                }
+            }
+            public string Phone
+            {
+                get { return _phone; }
+                set
+                {
+                    _phone = value;
+                    Console.WriteLine("Phone: " + _phone);
+                }
+            }
+            public double Budget
+            {
+                get { return _budget; }
+                set
+                {
+                    _budget = value;
+                    Console.WriteLine("Budget: " + _budget);
+                }
+            }
+            public Memento SaveMemento()
+            {
+                Console.WriteLine("\nSaving state --\n");
+                return new Memento(_name, _phone, _budget);
+            }
+
+            public void RestoreMemento(Memento memento)
+            {
+                Console.WriteLine("\nRestoring state --\n");
+                this.Name = memento.Name;
+                this.Phone = memento.Phone;
+                this.Budget = memento.Budget;
+            }
+        }
+        class Memento
+        {
+            private string _name;
+            private string _phone;
+            private double _budget;
+
+            public Memento(string name, string phone, double budget)
+            {
+                this._name = name;
+                this._phone = phone;
+                this._budget = budget;
+            }
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            public string Phone
+            {
+                get { return _phone; }
+                set { _phone = value; }
+            }
+
+            public double Budget
+            {
+                get { return _budget; }
+                set { _budget = value; }
+            }
+        }
+
+        // The 'Caretaker' class
+        class ProspectMemory
+        {
+            private Memento _memento;
+
+            public Memento Memento
+            {
+                set { _memento = value; }
+                get { return _memento; }
+            }
+        }
+    }
+}
---
+++ Memento/Memento_Structural.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    class Memento_Structural
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This structural code demonstrates the Memento pattern which temporary saves and restores another object's internal state.\n");
+            Originator o = new Originator();
+            o.State = "On";
+
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            o.State = "Off";
+
+            o.SetMemento(c.Memento);
+            /*
+            State = On
+            State = Off
+            Restoring state:
+            State = On
+             */
+        }
+        class Originator
+        {
+            private string _state;
+
+            public string State
+            {
+                get { return _state; }
+                set
+                {
+                    _state = value;
+                    Console.WriteLine("State = " + _state);
+                }
+            }
+
+            public Memento CreateMemento()
+            {
+                return (new Memento(_state));
+            }
+
+            public void SetMemento(Memento memento)
+            {
+                Console.WriteLine("Restoring state...");
+                State = memento.State;
+            }
+        }
+        class Memento
+        {
+            private string _state;
+
+            public Memento(string state)
+            {
+                this._state = state;
+            }
+
+            public string State
+            {
+                get { return _state; }
+            }
+        }
+        class Caretaker
+        {
+            private Memento _memento;
+
+            public Memento Memento
+            {
+                set { _memento = value; }
+                get { return _memento; }
+            }
+        }
+    }
+
+}
