f3d9001748303bbac922af2aa928c7ef9ae282c8
Stephen Printup <corky@corkysdevshop.com>
2020-06-16 11:13:07 -0600

    strategy pattern refactored into own classes

---
+++ Strategy/Strategy_Structural.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class Strategy_Structural
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This structural code demonstrates the Strategy pattern which encapsulates functionality in the form of an object. This allows clients to dynamically change algorithmic strategies.\n");
+            Context context;
+
+            context = new Context(new ConcreteStrategyA());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyB());
+            context.ContextInterface();
+
+            context = new Context(new ConcreteStrategyC());
+            context.ContextInterface();
+            /*
+            Called ConcreteStrategyA.AlgorithmInterface()
+            Called ConcreteStrategyB.AlgorithmInterface()
+            Called ConcreteStrategyC.AlgorithmInterface()
+             */
+        }
+        abstract class Strategy
+        {
+            public abstract void AlgorithmInterface();
+        }
+        class ConcreteStrategyA : Strategy
+        {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine("Called ConcreteStrategyA.AlgorithmInterface()");
+            }
+        }
+        class ConcreteStrategyB : Strategy
+        {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine("Called ConcreteStrategyB.AlgorithmInterface()");
+            }
+        }
+        class ConcreteStrategyC : Strategy
+        {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine("Called ConcreteStrategyC. AlgorithmInterface()");
+            }
+        }
+        class Context
+        {
+            private Strategy _strategy;
+
+            public Context(Strategy strategy)
+            {
+                this._strategy = strategy;
+            }
+
+            public void ContextInterface()
+            {
+                _strategy.AlgorithmInterface();
+            }
+        }
+    }
+}
---
+++ Strategy/Structural_Real World.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class Strategy_Real_World
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This real-world code demonstrates the Strategy pattern which encapsulates sorting algorithms in the form of sorting objects. This allows clients to dynamically change sorting strategies including Quicksort, Shellsort, and Mergesort.\n");
+            SortedList studentRecords = new SortedList();
+
+            studentRecords.Add("Samual");
+            studentRecords.Add("Jimmy");
+            studentRecords.Add("Sandra");
+            studentRecords.Add("Vivek");
+            studentRecords.Add("Anna");
+
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
+            /*
+            QuickSorted list
+             Anna
+             Jimmy
+             Samual
+             Sandra
+             Vivek
+
+            ShellSorted list
+             Anna
+             Jimmy
+             Samual
+             Sandra
+             Vivek
+
+            MergeSorted list
+             Anna
+             Jimmy
+             Samual
+             Sandra
+             Vivek             
+             */
+        }
+        abstract class SortStrategy
+        {
+            public abstract void Sort(List<string> list);
+        }
+
+        //The 'ConcreteStrategy' class
+        class QuickSort : SortStrategy
+        {
+            public override void Sort(List<string> list)
+            {
+                list.Sort();
+                Console.WriteLine("QuickSorted list ");
+            }
+        }
+
+        class ShellSort : SortStrategy
+        {
+            public override void Sort(List<string> list)
+            {
+                Console.WriteLine("ShellSorted list ");
+            }
+        }
+
+        class MergeSort : SortStrategy
+        {
+            public override void Sort(List<string> list)
+            {
+                Console.WriteLine("MergeSorted list ");
+            }
+        }
+
+        // The 'Context' class
+        class SortedList
+        {
+            private List<string> _list = new List<string>();
+            private SortStrategy _sortstrategy;
+
+            public void SetSortStrategy(SortStrategy sortstrategy)
+            {
+                this._sortstrategy = sortstrategy;
+            }
+
+            public void Add(string name)
+            {
+                _list.Add(name);
+            }
+
+            public void Sort()
+            {
+                _sortstrategy.Sort(_list);
+
+                foreach (string name in _list)
+                {
+                    Console.WriteLine(" " + name);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
