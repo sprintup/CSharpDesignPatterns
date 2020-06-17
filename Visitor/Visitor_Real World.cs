f3c74673b1d0082c393ab2951be526497f35f13f
Stephen Printup <corky@corkysdevshop.com>
2020-06-17 11:09:37 -0600

    visitor pattern refactored into its own classes

---
+++ Visitor/Visitor_Real World.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    class Visitor_Real_World
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This real-world code demonstrates the Visitor pattern in which two objects traverse a list of Employees and performs the same operation on each Employee. The two visitor objects define different operations -- one adjusts vacation days and the other income.\n");
+            Employees e = new Employees();
+            e.Attach(new Clerk());
+            e.Attach(new Director());
+            e.Attach(new President());
+
+            e.Accept(new IncomeVisitor());
+            e.Accept(new VacationVisitor());
+            /*
+            Clerk Hank's new income: $27,500.00
+            Director Elly's new income: $38,500.00
+            President Dick's new income: $49,500.00
+
+            Clerk Hank's new vacation days: 17
+            Director Elly's new vacation days: 19
+            President Dick's new vacation days: 24             
+             */
+        }
+        // The 'Visitor' interface
+        interface IVisitor
+        {
+            void Visit(Element element);
+        }
+        // A 'ConcreteVisitor' class
+        class IncomeVisitor : IVisitor
+        {
+            public void Visit(Element element)
+            {
+                Employee employee = element as Employee;
+
+                employee.Income *= 1.10;
+                Console.WriteLine("{0} {1}'s new income: {2:C}", employee.GetType().Name, employee.Name, employee.Income);
+            }
+        }
+        // A 'ConcreteVisitor' class
+        class VacationVisitor : IVisitor
+        {
+            public void Visit(Element element)
+            {
+                Employee employee = element as Employee;
+
+                employee.VacationDays += 3;
+                Console.WriteLine("{0} {1}'s new vacation days: {2}",
+                    employee.GetType().Name, employee.Name,
+                    employee.VacationDays);
+            }
+        }
+        // The 'Element' abstract class
+        abstract class Element
+        {
+            public abstract void Accept(IVisitor visitor);
+        }
+        // The 'ConcreteElement' class
+        class Employee : Element
+        {
+            private string _name;
+            private double _income;
+            private int _vacationDays;
+
+            public Employee(string name, double income, int vacationDays)
+            {
+                this._name = name;
+                this._income = income;
+                this._vacationDays = vacationDays;
+            }
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public double Income
+            {
+                get { return _income; }
+                set { _income = value; }
+            }
+
+            public int VacationDays
+            {
+                get { return _vacationDays; }
+                set { _vacationDays = value; }
+            }
+
+            public override void Accept(IVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+        // The 'ObjectStructure' class
+        class Employees
+        {
+            private List<Employee> _employees = new List<Employee>();
+
+            public void Attach(Employee employee)
+            {
+                _employees.Add(employee);
+            }
+
+            public void Detach(Employee employee)
+            {
+                _employees.Remove(employee);
+            }
+
+            public void Accept(IVisitor visitor)
+            {
+                foreach (Employee e in _employees)
+                {
+                    e.Accept(visitor);
+                }
+                Console.WriteLine();
+            }
+        }
+        // Three employee types
+        class Clerk : Employee
+        {
+            public Clerk() : base("Hank", 25000.0, 14)
+            {
+            }
+        }
+
+        class Director : Employee
+        {
+            public Director() : base("Elly", 35000.0, 16)
+            {
+            }
+        }
+
+        class President : Employee
+        {
+            public President() : base("Dick", 45000.0, 21)
+            {
+
+            }
+        }
+    }
+}
---
+++ Visitor/Visitor_Structural.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    class Visitor_Structural
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This structural code demonstrates the Visitor pattern in which an object traverses an object structure and performs the same operation on each node in this structure. Different visitor objects define different operations.\n");
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new ConcreteElementA());
+            o.Attach(new ConcreteElementB());
+
+            ConcreteVisitor1 v1 = new ConcreteVisitor1();
+            ConcreteVisitor2 v2 = new ConcreteVisitor2();
+
+            o.Accept(v1);
+            o.Accept(v2);
+            /*
+            ConcreteElementA visited by ConcreteVisitor1
+            ConcreteElementB visited by ConcreteVisitor1
+            ConcreteElementA visited by ConcreteVisitor2
+            ConcreteElementB visited by ConcreteVisitor2             
+             */
+        }
+        abstract class Visitor
+        {
+            public abstract void VisitConcreteElementA(ConcreteElementA concreteElementA);
+            public abstract void VisitConcreteElementB(ConcreteElementB concreteElementB);
+        }
+        class ConcreteVisitor1 : Visitor
+        {
+            public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+            {
+                Console.WriteLine("{0} visited by {1}", concreteElementA.GetType().Name, this.GetType().Name);
+            }
+
+            public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+            {
+                Console.WriteLine("{0} visited by {1}", concreteElementB.GetType().Name, this.GetType().Name);
+            }
+        }
+        class ConcreteVisitor2 : Visitor
+        {
+            public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+            {
+                Console.WriteLine("{0} visited by {1}", concreteElementA.GetType().Name, this.GetType().Name);
+            }
+            public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+            {
+                Console.WriteLine("{0} visited by {1}", concreteElementB.GetType().Name, this.GetType().Name);
+            }
+        }
+        abstract class Element1
+        {
+            public abstract void Accept(Visitor visitor);
+        }
+        class ConcreteElementA : Element1
+        {
+            public override void Accept(Visitor visitor)
+            {
+                visitor.VisitConcreteElementA(this);
+            }
+
+            public void OperationB() { }
+        }
+        class ConcreteElementB : Element1
+        {
+            public override void Accept(Visitor visitor)
+            {
+                visitor.VisitConcreteElementB(this);
+            }
+
+            public void OperationB() { }
+        }
+        class ObjectStructure
+        {
+            private List<Element1> _elements = new List<Element1>();
+
+            public void Attach(Element1 element)
+            {
+                _elements.Add(element);
+            }
+
+            public void Detach(Element1 element)
+            {
+                _elements.Remove(element);
+            }
+
+            public void Accept(Visitor visitor)
+            {
+                foreach (Element1 element in _elements)
+                {
+                    element.Accept(visitor);
+                }
+            }
+        }
+    }
+}
