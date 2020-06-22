e0d2147ecda5e5a61ad29269f9a8190403d90b58
Stephen Printup <corky@corkysdevshop.com>
2020-06-22 09:24:43 -0600

    facade upgrades

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -6,9 +6,10 @@ namespace Facade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! \nFacade: Provide a unified interface to a set of interfaces in a subsystem. Façade defines a higher-level interface that makes the subsystem easier to use.\n");
+            Console.WriteLine("Hello World! \nFacade: Provide a unified interface to a set of interfaces in a subsystem. Façade defines a higher-level interface that makes the subsystem easier to use.");
 
             Facade_Structural.Run();
+            Console.WriteLine("-----");
             Facade_RealWorld.Run();
 
             Console.ReadKey();
