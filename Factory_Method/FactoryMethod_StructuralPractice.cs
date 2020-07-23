8a020fa255eab6533dac2fdc65632f387305a565
Stephen Printup <corky@corkysdevshop.com>
2020-07-23 14:38:23 -0600

    Factory Method Structural Practice

---
+++ Factory_Method/FactoryMethod_StructuralPractice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory_Method_Practice
+{
+    class FactoryMethod_StructuralPractice
+    {
+        public static void Run()
+        {
+            Console.WriteLine("\nFactory Method Structural Practice");
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}", product.GetType().Name);
+            }
+        }
+
+        abstract class Creator 
+        {
+            public abstract Product FactoryMethod();
+        }
+
+        abstract class Product { }
+
+        class ConcreteCreatorA : Creator
+        {
+            public override Product FactoryMethod()
+            {
+                return new ConcreteProductA();
+            }
+        }
+
+        class ConcreteProductA : Product { }
+
+        class ConcreteCreatorB : Creator
+        {
+            public override Product FactoryMethod()
+            {
+                return new ConcreteProductB();
+            }
+        }
+
+        class ConcreteProductB : Product { }
+    }
+}
--- Factory_Method/Program.cs
+++ Factory_Method/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Factory_Method_Practice;
 
 namespace Factory_Method
 {
@@ -9,6 +10,7 @@ namespace Factory_Method
         {
             Console.WriteLine("Hello World! \nFactory Method: Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.");
             Factory_Method_Structural.Run();
+            FactoryMethod_StructuralPractice.Run();
             Console.WriteLine("-----");
             Factory_Method_RealWorld.Run();
             Console.ReadKey();
